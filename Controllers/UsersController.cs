bcdc49ab0d8988530e476b53fb5dbf0719c5bfd6
mi_ashutosh <ashutoshkokam@outlook.com>
2021-04-20 19:31:26 +0530

    Base REPO

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                int y = 0;
+                int x = 100 / y;
+                var users = _userService.GetAll();
+                return Ok(users);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+              
+            }
+           
+        }
+    }
+}
